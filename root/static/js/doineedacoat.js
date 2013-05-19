d0f5fac57a7945cde81df44a176de7990d47ce2a
Dominic Pain <dominic_pain@hotmail.com>
2013-05-19 21:04:59 +0100

    ensure length_of_stay gets passed to back end

--- root/static/js/doineedacoat.js
+++ root/static/js/doineedacoat.js
@@ -3,7 +3,10 @@ var localSearch = new GlocalSearch();
 
 
 function processAndRedirect(lat,lng) {
-    window.location.replace("http://localhost:3000/doineedacoat?lat=" + lat + "&lng=" + lng);
+    window.location.replace("http://localhost:3000/doineedacoat?lat=" + 
+        lat + "&lng=" + 
+        lng + "&length_of_stay=" + 
+        $("#length_of_stay_select :selected").val());
 }
 
 function usePointFromPostcode(postcode, callbackFunction) {
